8bcc748570b2731b6ce53ac0c4d03919cd8dd388
barak diker <barakdiker@gmail.com>
2022-11-06 15:21:58 +0200

    Added Version

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include <sstream>
 #include <thread>
 
-float ScaleFactor = 0.4;
+float ScaleFactor = 0.5;
 using namespace std::chrono_literals;
 
 struct Edge {
@@ -126,7 +126,7 @@ bool is_valid_movement(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud,
                        pcl::PointXYZ current_point, pcl::PointXYZ dest_point,
                        pcl::KdTreeFLANN<pcl::PointXYZ> &kdtree) {
   float jump_distance = 0.1 * ScaleFactor; // Magic number ?
-  float radius_search = 0.2 * ScaleFactor;
+  float radius_search = 0.25 * ScaleFactor;
   std::vector<pcl::PointXYZ> v_points_on_line =
       get_points_on_line(current_point, dest_point, jump_distance);
   for (pcl::PointXYZ &p : v_points_on_line) {
@@ -248,9 +248,25 @@ int main(int argc, char **argv) {
   lemon::ListGraph RRTGraph;
   lemon::ListGraph::NodeMap<pcl::PointXYZ> nodeToPoint(RRTGraph);
   // std::unordered_map<pcl::PointXYZ, lemon::ListGraph::Node> pointToNode;
-  pcl::PointXYZ knownPoint1 = {-0.264355, -0.105879 + 0.1, 0.166656};
-  pcl::PointXYZ knownPoint2 = {-0.281147, -0.032053 + 0.1, -0.252866};
-  pcl::PointXYZ knownPoint3 = {-0.661326, -0.038876 + 0.1, -0.242295};
+  // points2.pcd
+  // pcl::PointXYZ knownPoint1 = {-0.264355 + 0.4, -0.105879 + 0.1, 0.166656};
+  // pcl::PointXYZ knownPoint2 = {-0.281147, -0.032053 + 0.1, -0.252866};
+  // pcl::PointXYZ knownPoint3 = {-0.661326, -0.038876 + 0.1, -0.242295};
+  // points3.pcd
+  // pcl::PointXYZ knownPoint1 = {-0.152039, -0.112927 , 0.521806};
+  // pcl::PointXYZ knownPoint2 = {-0.0388202, 0.0144199 , -0.150134};
+  // pcl::PointXYZ knownPoint3 = {0.0739677 , 0.0088157 , -0.170298};
+  pcl::PointXYZ knownPoint1 = {0.114433, -0.0792644 + 0.0, 0.238077};
+  pcl::PointXYZ knownPoint2 = {0.0119525, -0.00770169 + 0.0, -0.0757213};
+  pcl::PointXYZ knownPoint3 = {0.548205, 0.0233536 + 0.0, -0.146354};
+
+  //  2.98735 -0.312701 0.234717
+  // 2.15991 -0.303712 0.337741
+  // 3.11331 -0.574892 0.515388
+
+  // pcl::PointXYZ knownPoint1 = {2.98735, -0.312701, 0.234717};
+  // pcl::PointXYZ knownPoint3 = {2.15991, -0.303712, 0.337741};
+  // pcl::PointXYZ knownPoint2 = {3.11331, -0.574892, 0.515388};
 
   if (argc < 2) {
     std::cout << "Please Enter pcd you want to analysis  e.g yam_data.pcd"
@@ -269,10 +285,11 @@ int main(int argc, char **argv) {
   pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
   kdtree.setInputCloud(cloud);
 
-  pcl::PointXYZ navigate_starting_point;
-  navigate_starting_point.x = knownPoint1.x;
-  navigate_starting_point.y = knownPoint1.y;
-  navigate_starting_point.z = knownPoint1.z;
+  pcl::PointXYZ navigate_starting_point = {0.114433, -0.0792644 + 0.0,
+                                           0.238077};
+  // navigate_starting_point.x = knownPoint1.x;
+  // navigate_starting_point.y = knownPoint1.y;
+  // navigate_starting_point.z = knownPoint1.z;
   std::vector<Edge> v_edges;
   pcl::PointCloud<pcl::PointXYZ>::Ptr navigate_data1(
       new pcl::PointCloud<pcl::PointXYZ>);
@@ -285,7 +302,7 @@ int main(int argc, char **argv) {
   //  std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with
   //  rd() std::uniform_real_distribution<> dis(-30.0, 30.0); float jump = 0.5;
 
-  float jump = 0.5 * ScaleFactor;
+  float jump = 0.8 * ScaleFactor;
   for (int i = 0; i < 2000; i++) {
     pcl::PointXYZ point_rand = getRandomPointOnPlaneDefBy3Points(
         knownPoint1, knownPoint2, knownPoint3);
@@ -336,7 +353,7 @@ int main(int argc, char **argv) {
   for (pcl::PointXYZ p : *navigate_data1) {
     std::stringstream ss;
     ss << "PointNavigate" << index;
-    viewer->addSphere(p, 0.15 * ScaleFactor, 0.9, 0.2, 0.0, ss.str());
+    viewer->addSphere(p, 0.2 * ScaleFactor, 0.9, 0.2, 0.0, ss.str());
     index++;
   }
 
@@ -375,7 +392,7 @@ int main(int argc, char **argv) {
 
       std::stringstream ss;
       ss << "PointNavigatePath" << index;
-      viewer->addSphere(nodeToPoint[prev], 0.15 * ScaleFactor, 0.1, 0.2, 0.9,
+      viewer->addSphere(nodeToPoint[prev], 0.2 * ScaleFactor, 0.1, 0.2, 0.9,
                         ss.str());
       index++;
     }
@@ -386,7 +403,33 @@ int main(int argc, char **argv) {
                               navigate_starting_point.y,
                               navigate_starting_point.z);
 
+  // pcl::PointXYZ p_helper{0.0867836,0.031304,0.0313494};
+  //  pcl::PointXYZ p_helper{0.152039, -0.112927,0.521806};
+  // pcl::PointXYZ p_helper{-0.804435 , -0.00937385 , 0.289798};
   // Print Path
+  // viewer->addSphere(pcl::PointXYZ(-0.804435, -0.00937385, 0.289798),
+  //                   0.15 * ScaleFactor, 1.0, 1.0, 1.0, "SHPERE121");
+  // viewer->addSphere(pcl::PointXYZ(-0.684641, -0.0372174, 0.37567),
+  //                   0.15 * ScaleFactor, 1.0, 1.0, 0.9, "SHPERE122");
+  // viewer->addSphere(pcl::PointXYZ(-0.568991, -0.0657816, 0.466824),
+  //                   0.15 * ScaleFactor, 1.0, 1.0, 0.8, "SHPERE123");
+  // viewer->addSphere(pcl::PointXYZ(-0.460346, -0.0953637, 0.565926),
+  //                   0.15 * ScaleFactor, 1.0, 1.0, 0.7, "SHPERE124");
+  // viewer->addSphere(pcl::PointXYZ(-0.342639, -0.123583, 0.65452),
+  //                   0.15 * ScaleFactor, 1.0, 1.0, 0.6, "SHPERE125");
+  // viewer->addSphere(pcl::PointXYZ(-0.193642, -0.138288, 0.663672),
+  //                   0.15 * ScaleFactor, 1.0, 1.0, 0.5, "SHPERE126");
+  // viewer->addSphere(pcl::PointXYZ(-0.152039, -0.112927, 0.521806),
+  //                   0.15 * ScaleFactor, 1.0, 1.0, 0.4, "SHPERE127");
+  // viewer->addSphere(pcl::PointXYZ(0, 0, 0), 0.15 * ScaleFactor, 0.3, 0.5,
+  // 0.5,
+  //                   "SHPERE127asdfasDolevf");
+
+  pcl::PointXYZ knownPoint4 = {-0.264355 + 0.6, -0.105879 + 0.1, 0.166656};
+  viewer->addSphere(pcl::PointXYZ(0.320001, 0.028691, -0.409289),
+                    0.3 * ScaleFactor, 0.15, 0.5, 0.5, "SHPERE127asdfasDolevf");
+  viewer->addSphere(knownPoint4, 0.15 * ScaleFactor, 0.3, 0.5, 0.5,
+                    "SHPERE127asdfasDolevf1");
 
   //--------------------
   // -----Main loop-----
