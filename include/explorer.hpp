0ca467e560bb7dccc0e8599cc9ee19c51cf9e0e3
Ido Talmor <ifw.talmor@gmail.com>
2023-09-05 18:59:33 +0300

    more formating changes, passes compile

--- include/explorer.hpp
+++ include/explorer.hpp
@@ -7,34 +7,35 @@
 #include <pcl/impl/point_types.hpp>
 #include <vector>
 
-class Explorer {
+class Explorer
+{
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
     std::array<pcl::PointXYZ, 3> known_points;
     std::vector<pcl::PointXYZ> RRT_points;
     bool got_plane_of_flight = false;
 
-   public:
+  public:
     /// Keeps k best paths we found
     std::vector<std::vector<pcl::PointXYZ>> best_paths;
 
     explicit Explorer(
-        const std::vector<Eigen::Matrix<double, 3, 1>>& map_points);
+        const std::vector<Eigen::Matrix<double, 3, 1>> &map_points);
 
-    explicit Explorer(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud);
+    explicit Explorer(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud);
 
     /**
      * @brief Add points from ORB_SLAM to the internal point cloud
      */
     void add_points_to_cloud(
-        const std::vector<Eigen::Matrix<double, 3, 1>>& map_points);
+        const std::vector<Eigen::Matrix<double, 3, 1>> &map_points);
     std::vector<pcl::PointXYZ> get_last_graph();
 
     void set_cloud_points(
-        const std::vector<Eigen::Matrix<double, 3, 1>>& map_points);
+        const std::vector<Eigen::Matrix<double, 3, 1>> &map_points);
 
-    void set_plane_of_flight(const pcl::PointXYZ& known_point1,
-                             const pcl::PointXYZ& known_point2,
-                             const pcl::PointXYZ& known_point3);
+    void set_plane_of_flight(const pcl::PointXYZ &known_point1,
+                             const pcl::PointXYZ &known_point2,
+                             const pcl::PointXYZ &known_point3);
 
     bool is_set_plane_of_flight() const { return got_plane_of_flight; }
 
@@ -42,8 +43,8 @@ class Explorer {
     /**
      * @brief Use the path builder API to get a path to the unknown
      */
-    std::vector<pcl::PointXYZ> get_points_to_unknown(
-        const pcl::PointXYZ& start_point);
+    std::vector<pcl::PointXYZ>
+    get_points_to_unknown(const pcl::PointXYZ &start_point);
 };
 
-#endif  // EXPLORER_H_
+#endif // EXPLORER_H_
--- src/explorer.cc
+++ src/explorer.cc
@@ -4,46 +4,56 @@
 
 #include "path_builder.hpp"
 
-Explorer::Explorer(const std::vector<Eigen::Matrix<double, 3, 1>>& map_points)
-    : cloud(new pcl::PointCloud<pcl::PointXYZ>) {
+Explorer::Explorer(const std::vector<Eigen::Matrix<double, 3, 1>> &map_points)
+    : cloud(new pcl::PointCloud<pcl::PointXYZ>)
+{
     add_points_to_cloud(map_points);
 }
 
-Explorer::Explorer(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud)
-    : cloud(new pcl::PointCloud<pcl::PointXYZ>(*cloud)) {}
+Explorer::Explorer(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
+    : cloud(new pcl::PointCloud<pcl::PointXYZ>(*cloud))
+{
+}
 
 void Explorer::set_cloud_points(
-    const std::vector<Eigen::Matrix<double, 3, 1>>& map_points) {
+    const std::vector<Eigen::Matrix<double, 3, 1>> &map_points)
+{
     cloud->clear();
     add_points_to_cloud(map_points);
 }
 
 void Explorer::add_points_to_cloud(
-    const std::vector<Eigen::Matrix<double, 3, 1>>& map_points) {
-    for (auto p : map_points) {
+    const std::vector<Eigen::Matrix<double, 3, 1>> &map_points)
+{
+    for (auto p : map_points)
+    {
         cloud->push_back(pcl::PointXYZ(static_cast<float>(p.x()),
                                        static_cast<float>(p.y()),
                                        static_cast<float>(p.z())));
     }
 }
 
-void Explorer::set_plane_of_flight(const pcl::PointXYZ& known_point1,
-                                   const pcl::PointXYZ& known_point2,
-                                   const pcl::PointXYZ& known_point3) {
+void Explorer::set_plane_of_flight(const pcl::PointXYZ &known_point1,
+                                   const pcl::PointXYZ &known_point2,
+                                   const pcl::PointXYZ &known_point3)
+{
     got_plane_of_flight = true;
 
     known_points = {known_point1, known_point2, known_point3};
 }
 
-std::vector<pcl::PointXYZ> Explorer::get_last_graph() {
+std::vector<pcl::PointXYZ> Explorer::get_last_graph()
+{
     std::vector<pcl::PointXYZ> last_graph = RRT_points;
     RRT_points.clear();
     return last_graph;
 }
 
-std::vector<pcl::PointXYZ> Explorer::get_points_to_unknown(
-    const pcl::PointXYZ& start_point) {
-    if (!got_plane_of_flight) {
+std::vector<pcl::PointXYZ>
+Explorer::get_points_to_unknown(const pcl::PointXYZ &start_point)
+{
+    if (!got_plane_of_flight)
+    {
         std::cerr << "Explorer: Expected plane of flight to be set"
                   << std::endl;
         return {};
